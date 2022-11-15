429ece0708093fd1a01145654d3b4b63118c01fc
inadatoshiki <toshi.4959@gmail.com>
2022-11-15 17:20:48 +0900

    バリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,16 @@ class Item < ApplicationRecord
   belongs_to :days_ship
   belongs_to :prefecture
   belongs_to :who_pay
+
+  validates :name, presence: true
+  validates :content, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :who_pay_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :days_ship_id, numericality: { other_than: 1 , message: "can't be blank"}
+  VALID_PRICEL_HALF =  /\A[0-9]+\z/
+  validates :price, presence: true, format: {with: VALID_PRICEL_HALF},length: {minimum: 3, maxinum: 7},numericality: { only_integer: true,
+    greater_than: 300, less_than: 10000000}
+  validates :image, presence: true
 end
--- db/migrate/20221115055051_create_items.rb
+++ db/migrate/20221115055051_create_items.rb
@@ -3,11 +3,11 @@ class CreateItems < ActiveRecord::Migration[6.0]
     create_table :items do |t|
       t.string :name, null: false
       t.text :content, null: false
-      t.integer :cayegory_id, null: false, numericality: { other_than: 1 , message: "can't be blank"}
-      t.integer :condition_id, null: false, numericality: { other_than: 1 , message: "can't be blank"}
-      t.integer :who_pay_id, null: false, numericality: { other_than: 1 , message: "can't be blank"}
-      t.integer :prefecture_id, null: false, numericality: { other_than: 1 , message: "can't be blank"}
-      t.integer :days_ship_id, null: false, numericality: { other_than: 1 , message: "can't be blank"}
+      t.integer :cayegory_id, null: false
+      t.integer :condition_id, null: false
+      t.integer :who_pay_id, null: false
+      t.integer :prefecture_id, null: false
+      t.integer :days_ship_id, null: false
       t.integer :price, null: false
       t.references :user, null: false, foreign_key: true
       t.timestamps
