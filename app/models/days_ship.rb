e4c6e94c852c69818893fc3f061686d95ecfad80
inadatoshiki <toshi.4959@gmail.com>
2022-11-15 15:55:47 +0900

    ActiveHashの導入

---
+++ app/models/days_ship.rb
@@ -0,0 +1,12 @@
+class Days_ship < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: '1〜2日で発送' },
+    { id: 2, name: '2〜3日で発送' },
+    { id: 2, name: '4〜7日で発送' },
+    ]
+
+  include ActiveHash::Associations
+  has_many :items
+
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :days_ship
+  belongs_to :prefecture
+  belongs_to :who_pay
 end
