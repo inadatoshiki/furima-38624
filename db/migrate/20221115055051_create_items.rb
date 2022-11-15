f9682cb33347e009a2b7d52cb3a9ac058710493d
inadatoshiki <toshi.4959@gmail.com>
2022-11-15 16:58:21 +0900

    itemsテーブルの作成

--- db/migrate/20221115055051_create_items.rb
+++ db/migrate/20221115055051_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      
+      t.string :name, null: false
+      t.text :content, null: false
+      t.integer :cayegory_id, null: false, numericality: { other_than: 1 , message: "can't be blank"}
+      t.integer :condition_id, null: false, numericality: { other_than: 1 , message: "can't be blank"}
+      t.integer :who_pay_id, null: false, numericality: { other_than: 1 , message: "can't be blank"}
+      t.integer :prefecture_id, null: false, numericality: { other_than: 1 , message: "can't be blank"}
+      t.integer :days_ship_id, null: false, numericality: { other_than: 1 , message: "can't be blank"}
+      t.integer :price, null: false
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
