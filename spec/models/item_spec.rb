0506c1d7058a5f84566d3553c1cb0a4ecb5722f7
inadatoshiki <toshi.4959@gmail.com>
2022-11-16 18:14:41 +0900

    修正　テストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -34,20 +34,50 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
+      it 'カテゴリーに「---」が選択されている場合は出品できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")      
+      end  
       it '商品の状態の情報がないと出品できないこと' do
         @item.condition_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank")
-      end          
+      end 
+      it '商品の状態に「---」が選択されている場合は出品できない' do
+        @item.condition_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end           
       it '配送料の負担の情報がないと出品できないこと' do
         @item.payer_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Payer can't be blank")
       end
-      it '発送元の情報がないと出品できないこと' do
+      it '・配送料の負担に「---」が選択されている場合は出品できない' do
+        @item.payer_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Payer can't be blank")
+      end  
+      it '発送元の地域の情報がないと出品できないこと' do
         @item.prefecture_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end  
+      it '・発送元の地域に「---」が選択されている場合は出品できない' do
+        @item.prefecture_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")     
+      end  
+      it '発送までの日数の情報がないと出品できないこと' do
+        @item.days_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days can't be blank")
+      end  
+      it '発送までの日数に「---」が選択されている場合は出品できない' do
+        @item.days_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days can't be blank")       
       end  
       it '価格がないと出品できないこと' do
         @item.price = ''
@@ -64,11 +94,16 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than 10000000")
       end
-      it '価格は全角数字では保存可能できないこと' do
+      it '価格は全角数字では保存できないこと' do
         @item.price = '３００'
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
-      end          
+      end   
+      it 'userが紐付いていなければ出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end  
     end
 
   end
