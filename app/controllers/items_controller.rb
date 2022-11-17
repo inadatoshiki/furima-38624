2e805ae715091fecf2bd3de80485aec2d08cb215
inadatoshiki <toshi.4959@gmail.com>
2022-11-17 17:58:32 +0900

    修正　編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :contributor_confirmation, only: [:edit, :update,]
+  before_action :set_item, only: [:show, :edit, :update,]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end 
 
   def show
-    @item = Item.find(params[:id])
   end  
 
   def edit
-    @item = Item.find(params[:id])
   end
   
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -47,4 +45,7 @@ class ItemsController < ApplicationController
     redirect_to root_path unless current_user == @item.user
   end
   
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
