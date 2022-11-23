db8b793516f2c12b9424e1d20f0983a07f44cb9c
inadatoshiki <toshi.4959@gmail.com>
2022-11-23 11:34:57 +0900

    エラーメッセージの記述

--- app/models/order_buyer.rb
+++ app/models/order_buyer.rb
@@ -5,11 +5,11 @@ class OrderBuyer
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
     validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :city
     validates :address
-    validates :phone, format: {with: /\A[0-9]{10,11}\z/ }
+    validates :phone, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid. Input only number" }
   end  
 
   def save
@@ -17,4 +17,3 @@ class OrderBuyer
     Buyer.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone: phone, order_id: order.id)
   end
 end  
-
